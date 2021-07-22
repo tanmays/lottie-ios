0c9dd709c0ed3c2d8c67a8b34ad249a28d32da09
Tanmay <tanmay@tanmay.me>
2021-07-23 01:36:03 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
     platforms: [.iOS(.v9)],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
-        .library(name: "Lottie", targets: ["Lottie"])
+        .library(name: "LottieMac", targets: ["LottieMac"])
     ],
     targets: [
         .target(
-            name: "Lottie",
+            name: "LottieMac",
             path: "lottie-swift/src",
             exclude: ["Public/MacOS"]
         )
