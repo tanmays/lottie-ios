1d8124e11829196245d88d222434def3e89ad8cf
Tanmay <tanmay@tanmay.me>
2021-07-23 01:37:01 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Lottie",
+    name: "LottieMac",
     platforms: [.macOS("10.10")],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
-        .library(name: "Lottie", targets: ["Lottie"])
+        .library(name: "LottieMac", targets: ["LottieMac"])
     ],
     targets: [
         .target(
-            name: "Lottie",
+            name: "LottieMac",
             path: "lottie-swift/src",
             exclude: ["Public/iOS"]
         )
